96c8f976a99de95fe1b4cb9b5948f301180dbfb9
fahmidyt <fmidyt@gmail.com>
2022-11-28 17:40:08 +0700

    refactor: adjusting login page for mobile

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,10 +17,10 @@
         <div class="flex-1 h-full w-full overflow-hidden">
             <div class="flex flex-col overflow-y-auto md:flex-row h-screen">
                 <div class="flex items-center justify-center md:w-1/3 md:p-6 w-full">
-                    <div class="md:max-w-lg">
+                    <div class="md:max-w-lg p-4 md:p-0">
                         <img class="w-[180px] mx-auto mb-6" src="{{ URL('assets/img/logo-umby.png') }}" />
-                        <h1 class="mb-2 text-3xl text-gray-700">Masuk ke <span class="font-semibold">SIA</span></h1>
-                        <p class="mb-4 text-cool-gray-400">Sistem Informasi Akademik <span class="font-semibold">Univeritas Mercubuana</span></p>
+                        <h1 class="mb-2 text-2xl md:text-3xl text-gray-600">Masuk ke <span class="font-semibold">SIA</span></h1>
+                        <p class="mb-4 text-cool-gray-400 text-sm md:text-base">Sistem Informasi Akademik <span class="font-semibold">Univeritas Mercubuana Yogyakarta</span></p>
 
                         <!-- Form section -->
                         <form>
@@ -32,7 +32,7 @@
                                 <input type="password" name="password" placeholder="Password" class="form-input rounded block w-full mt-1 focus:border-blue-400 focus:outline-none focus:shadow-outline-blue my-4 border-cool-gray-100 text-gray-600 py-4" />
                             </label>
 
-                            <div class="flex mb-12">
+                            <div class="flex mb-10">
                                 <label class="block">
                                     <input type="checkbox" id="toggle-password" class="form-checkbox rounded">
                                     <span class="text-cool-gray-500">Tampilkan password</span>
@@ -44,9 +44,9 @@
                                 </p>
                             </div>
 
-                            <a class="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-blue-500 border border-transparent rounded-lg active:bg-blue-500 hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue" href="../index.html">
+                            <button type="submit" class="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-blue-500 border border-transparent rounded-lg active:bg-blue-500 hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue" href="../index.html">
                                 Log in
-                            </a>
+                            </button>
                         </form>
                         <div class="block">
                             <a class="text-sm font-medium text-cool-gray-400 underline mx-1" href="./forgot-password.html">
