428b8b8a3e595e0f58b67fbccf4132054bf571b6
fahmidyt <fmidyt@gmail.com>
2022-11-30 06:01:47 +0700

    refactor: page portal mahasiswa

--- resources/views/portal/mahasiswa.blade.php
+++ resources/views/portal/mahasiswa.blade.php
@@ -1,349 +1,126 @@
-@section('title', 'Mahasiswa')
-@include('layouts.header')
-<style>
-    /*Form fields*/
-    .dataTables_wrapper select,
-    .dataTables_wrapper .dataTables_filter input {
-        color: #4a5568;
-        /*text-gray-700*/
-        padding-left: 1rem;
-        /*pl-4*/
-        padding-right: 1rem;
-        /*pl-4*/
-        padding-top: .5rem;
-        /*pl-2*/
-        padding-bottom: .5rem;
-        /*pl-2*/
-        line-height: 1.25;
-        /*leading-tight*/
-        border-width: 2px;
-        /*border-2*/
-        border-radius: .25rem;
-        border-color: #edf2f7;
-        /*border-gray-200*/
-        background-color: #edf2f7;
-        /*bg-gray-200*/
-    }
+@extends('layouts.default')
+@section('title', 'Portal Mahasiswa')
 
-    /*Row Hover*/
-    table.dataTable.hover tbody tr:hover,
-    table.dataTable.display tbody tr:hover {
-        background-color: #ebf4ff;
-        /*bg-indigo-100*/
-    }
+@section('contents')
 
-    /*Pagination Buttons*/
-    .dataTables_wrapper .dataTables_paginate .paginate_button {
-        font-weight: 700;
-        /*font-bold*/
-        border-radius: .25rem;
-        /*rounded*/
-        border: 1px solid transparent;
-        /*border border-transparent*/
-    }
-
-    /*Pagination Buttons - Current selected */
-    .dataTables_wrapper .dataTables_paginate .paginate_button.current {
-        color: #fff !important;
-        /*text-white*/
-        box-shadow: 0 1px 3px 0 rgba(0, 0, 0, .1), 0 1px 2px 0 rgba(0, 0, 0, .06);
-        /*shadow*/
-        font-weight: 700;
-        /*font-bold*/
-        border-radius: .25rem;
-        /*rounded*/
-        background: #667eea !important;
-        /*bg-indigo-500*/
-        border: 1px solid transparent;
-        /*border border-transparent*/
-    }
-
-    /*Pagination Buttons - Hover */
-    .dataTables_wrapper .dataTables_paginate .paginate_button:hover {
-        color: #fff !important;
-        /*text-white*/
-        box-shadow: 0 1px 3px 0 rgba(0, 0, 0, .1), 0 1px 2px 0 rgba(0, 0, 0, .06);
-        /*shadow*/
-        font-weight: 700;
-        /*font-bold*/
-        border-radius: .25rem;
-        /*rounded*/
-        background: #667eea !important;
-        /*bg-indigo-500*/
-        border: 1px solid transparent;
-        /*border border-transparent*/
-    }
-
-    /*Add padding to bottom border */
-    table.dataTable.no-footer {
-        border-bottom: 1px solid #e2e8f0;
-        /*border-b-1 border-gray-300*/
-        margin-top: 0.75em;
-        margin-bottom: 0.75em;
-    }
-
-    /*Change colour of responsive icon*/
-    table.dataTable.dtr-inline.collapsed>tbody>tr>td:first-child:before,
-    table.dataTable.dtr-inline.collapsed>tbody>tr>th:first-child:before {
-        background-color: #667eea !important;
-        /*bg-indigo-500*/
-    }
-</style>
-
-<style>
-    .button {
-        background-color: #4CAF50;
-        /* Green */
-        border: none;
-        color: white;
-        padding: 8px 16px;
-        text-align: center;
-        text-decoration: none;
-        display: inline-block;
-        font-size: 14px;
-        margin: 2px 1px;
-        transition-duration: 0.4s;
-        cursor: pointer;
-        border-radius: 8px;
-    }
-
-    .button1 {
-        background-color: #4CAF50;
-        color: white;
-        border: 2px solid #4CAF50;
-    }
-
-    .button1:hover {
-        background-color: white;
-        color: black;
-    }
-
-    .button2 {
-        background-color: #008CBA;
-        color: white;
-        border: 2px solid #008CBA;
-    }
-
-    .button2:hover {
-        background-color: white;
-        color: black;
-    }
-
-    .button3 {
-        background-color: #f44336;
-        color: white;
-        border: 2px solid #f44336;
-    }
-
-    .button3:hover {
-        background-color: white;
-        color: black;
-    }
-
-    .button4 {
-        background-color: white;
-        color: black;
-        border: 2px solid #e7e7e7;
-    }
-
-    .button4:hover {
-        background-color: #e7e7e7;
-    }
-
-    .button5 {
-        background-color: white;
-        color: black;
-        border: 2px solid #555555;
-    }
-
-    .button5:hover {
-        background-color: #555555;
-        color: white;
-    }
-</style>
-<main class="h-full pb-16 overflow-y-auto">
-    <div class="container px-6 mx-auto grid">
-        <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
-            Mahasiswa
-        </h2>
-        <!-- General elements -->
-        <h4 class="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
-            Data Mahasiswa UMBY
-        </h4>
-        <div class="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800">
-            <div class="container mx-auto">
-                <div class="flex flex-col">
-                    Test
-                </div>
-            </div>
+    <x-card class="mb-6">
+        FILTER
+    </x-card>
+    <x-card title="Mahasiswa" class="display nowrap w-full">
+        <div class="flex mb-4">
+            <x-button size="sm" color="blue" as-link={{ true }} href="#" class="mx-2"><i
+                    data-feather="plus" width="16"></i>&nbsp;Tambah</x-button>
+            <x-button size="sm" color="green" as-link={{ true }} href="#" class="mx-2"><i
+                    data-feather="printer" width="16"></i>&nbsp;Cetak</x-button>
+            <x-button size="sm" color="white" as-link={{ true }} href="#"
+                class="mx-2 text-cool-gray-800 border-cool-gray-400"><i data-feather="refresh-cw"
+                    width="16"></i>&nbsp;Reset</x-button>
+            <x-button size="sm" color="white" as-link={{ true }} href="#"
+                class="mx-2  text-cool-gray-800 border-cool-gray-400"><i data-feather="mail" width="16"></i>&nbsp;Tulis
+                Email</x-button>
         </div>
-        <div class="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800">
-            <div class="flex flex-col flex-wrap mb-8 space-y-4 md:flex-row md:items-end md:space-x-4">
-                <div>
-                    <button class="button button1">
-                        <i class="fas fa-plus"></i>
-                        <span>&nbsp; Tambah</span>
-                    </button>
-                </div>
-                <div>
-                    <button class="flex items-center justify-between px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
-                        <i class="fa-solid fa-rotate"></i>
-                        <span>&nbsp; Reset</span>
-                    </button>
-                </div>
-                <div>
-                    <button class="flex items-center justify-between px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
-                        <i class="fa-solid fa-print"></i>
-                        <span>&nbsp; Cetak</span>
-                    </button>
-                </div>
-                <div>
-                    <button class="flex items-center justify-between px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
-                        <i class="fa-regular fa-envelope"></i>
-                        <span>&nbsp; Tulis Email</span>
-                    </button>
-                </div>
-            </div>
+        <table id="mahasiswa-table" class="hover cell-border stripe  order-column"></table>
 
-            <div class="container w-full md:w-4/5 xl:w-3/5  mx-auto px-2">
-                <table id="example" class="stripe hover" style="width:100%; padding-top: 1em;  padding-bottom: 1em;">
-                    <thead>
-                        <tr>
-                            <th data-priority="1">Name</th>
-                            <th data-priority="2">Position</th>
-                            <th data-priority="3">Office</th>
-                            <th data-priority="4">Age</th>
-                            <th data-priority="5">Start date</th>
-                            <th data-priority="6">Salary</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Tiger Nixon</td>
-                            <td>System Architect</td>
-                            <td>Edinburgh</td>
-                            <td>61</td>
-                            <td>2011/04/25</td>
-                            <td>$320,800</td>
-                        </tr>
-                        <tr>
-                            <td>Donna Snider</td>
-                            <td>Customer Support</td>
-                            <td>New York</td>
-                            <td>27</td>
-                            <td>2011/01/25</td>
-                            <td>$112,000</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-            <!--/Card-->
-        </div>
-        <!-- Validation inputs -->
-        <h4 class="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
-            Validation
-        </h4>
-        <div class="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800">
-            <!-- Invalid input -->
-            <label class="block text-sm">
-                <span class="text-gray-700 dark:text-gray-400">
-                    Invalid input
-                </span>
-                <input class="block w-full mt-1 text-sm border-red-600 dark:text-gray-300 dark:bg-gray-700 focus:border-red-400 focus:outline-none focus:shadow-outline-red form-input" placeholder="Jane Doe" />
-                <span class="text-xs text-red-600 dark:text-red-400">
-                    Your password is too short.
-                </span>
-            </label>
+    </x-card>
+@stop
 
-            <!-- Valid input -->
-            <label class="block mt-4 text-sm">
-                <span class="text-gray-700 dark:text-gray-400">
-                    Valid input
-                </span>
-                <input class="block w-full mt-1 text-sm border-green-600 dark:text-gray-300 dark:bg-gray-700 focus:border-green-400 focus:outline-none focus:shadow-outline-green form-input" placeholder="Jane Doe" />
-                <span class="text-xs text-green-600 dark:text-green-400">
-                    Your password is strong.
-                </span>
-            </label>
+@section('scripts')
+    <script>
+        const dataSet = [{
+                id: 1,
+                nim: '16011124',
+                nama: 'RONI PARTAMA',
+                stat: 'A',
+                angkatan: 2016,
+                asal: 'SMU -- GEL. 2',
+                jurusan: 'Agroteknologi',
+                jenis: 'R2',
+                kampus: 'Kampus Pusat'
+            },
+            {
+                id: 1,
+                nim: '16031003',
+                nama: 'MUGIONO',
+                stat: 'A',
+                angkatan: 2016,
+                asal: 'SMU -- GEL. 2',
+                jurusan: 'Agroteknologi',
+                jenis: 'R2',
+                kampus: 'Kampus Pusat'
+            },
+            {
+                id: 1,
+                nim: '16031089',
+                nama: 'SARPANDI',
+                stat: 'A',
+                angkatan: 2016,
+                asal: 'SMU -- GEL. 2',
+                jurusan: 'Agroteknologi',
+                jenis: 'R2',
+                kampus: 'Kampus Pusat'
+            },
+        ]
 
-            <!-- Helper text -->
-            <label class="block mt-4 text-sm">
-                <span class="text-gray-700 dark:text-gray-400">
-                    Helper text
-                </span>
-                <input class="block w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input" placeholder="Jane Doe" />
-                <span class="text-xs text-gray-600 dark:text-gray-400">
-                    Your password must be at least 6 characters long.
-                </span>
-            </label>
-        </div>
+        $(document).ready(function() {
+            $('#mahasiswa-table').DataTable({
+                data: dataSet,
+                columns: [{
+                        data: 'id',
+                        render: (data, type, row, meta) => meta.row + meta.settings._iDisplayStart + 1,
+                        title: "No."
+                    },
+                    {
+                        data: 'nim',
+                        title: 'NIM'
+                    },
+                    {
+                        data: 'nama',
+                        title: 'Nama Mahasiswa'
+                    },
+                    {
+                        data: 'stat',
+                        title: 'Stat.'
+                    },
+                    {
+                        data: 'angkatan',
+                        title: 'Angk.'
+                    },
+                    {
+                        data: 'asal',
+                        title: 'Asal'
+                    },
+                    {
+                        data: 'jurusan',
+                        title: 'Jurusan'
+                    },
+                    {
+                        data: 'jenis',
+                        title: 'Jenis'
+                    },
+                    {
+                        data: 'kampus',
+                        title: 'Kampus'
+                    },
+                    {
+                        data: 'id',
+                        title: 'Aksi',
+                        // TODO: FIX THIS
+                        render: (data) => {
+                            const btnClass = (color = "gray")  => `inline-flex items-center px-2 text-xs rounded my-2 font-medium leading-5 text-center text-white transition-colors duration-150 bg-${color}-500 border border-transparent active:bg-${color}-500 hover:bg-${color}-600 focus:outline-none focus:shadow-outline-${color} mx-1`
+                            const btn = `
+                            <div class="flex justify-center">
+                            <a href="#" class="${btnClass()}"><i data-feather="mail" width="16"></i></a>
+                            <a href="#" class="${btnClass()}"><i data-feather="edit" width="16"></i></a>
+                            <a href="#" class="${btnClass("red")}"><i data-feather="trash" width="16"></i></a>
+                            </div>
+                            `
+                            feather.replace()
 
-        <!-- Inputs with icons -->
-        <h4 class="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
-            Icons
-        </h4>
-        <div class="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800">
-            <label class="block text-sm">
-                <span class="text-gray-700 dark:text-gray-400">Icon left</span>
-                <!-- focus-within sets the color for the icon when input is focused -->
-                <div class="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400">
-                    <input class="block w-full pl-10 mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input" placeholder="Jane Doe" />
-                    <div class="absolute inset-y-0 flex items-center ml-3 pointer-events-none">
-                        <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                            <path d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
-                        </svg>
-                    </div>
-                </div>
-            </label>
-
-            <label class="block mt-4 text-sm">
-                <span class="text-gray-700 dark:text-gray-400">Icon right</span>
-                <!-- focus-within sets the color for the icon when input is focused -->
-                <div class="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400">
-                    <input class="block w-full pr-10 mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input" placeholder="Jane Doe" />
-                    <div class="absolute inset-y-0 right-0 flex items-center mr-3 pointer-events-none">
-                        <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                            <path d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
-                        </svg>
-                    </div>
-                </div>
-            </label>
-        </div>
-
-        <!-- Inputs with buttons -->
-        <h4 class="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
-            Buttons
-        </h4>
-        <div class="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800">
-            <label class="block text-sm">
-                <span class="text-gray-700 dark:text-gray-400">
-                    Button left
-                </span>
-                <div class="relative">
-                    <input class="block w-full pl-20 mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input" placeholder="Jane Doe" />
-                    <button class="absolute inset-y-0 px-4 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-l-md active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray">
-                        Click
-                    </button>
-                </div>
-            </label>
-
-            <label class="block mt-4 text-sm">
-                <span class="text-gray-700 dark:text-gray-400">
-                    Button right
-                </span>
-                <div class="relative text-gray-500 focus-within:text-purple-600">
-                    <input class="block w-full pr-20 mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input" placeholder="Jane Doe" />
-                    <button class="absolute inset-y-0 right-0 px-4 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-r-md active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
-                        Click
-                    </button>
-                </div>
-            </label>
-        </div>
-    </div>
-</main>
-@include('layouts.footer')
-<script>
-    $(document).ready(function() {
-        $('#example').DataTable();
-    });
-</script>
+                            return btn
+                        }
+                    }
+                ]
+            })
+        })
+    </script>
+@endsection
