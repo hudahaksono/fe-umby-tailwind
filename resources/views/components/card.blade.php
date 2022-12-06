132d2e4d5b8165caeb2eb7471a65e746181d5db8
fahmidyt <fmidyt@gmail.com>
2022-12-06 07:52:20 +0700

    refactor: add button slot in card component

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,8 +1,17 @@
 <div class="min-w-0 p-4 bg-white rounded-lg shadow-lg {{ $class }}">
-    @if (isset($title))
-        <h4 class="text-lg mb-4 font-semibold text-gray-600">
-            {{ $title }}
-        </h4>
+    <div class="flex items-center mb-4">
+        @if (isset($title))
+            <h4 class="text-lg font-semibold text-gray-600">
+                {{ $title }}
+            </h4>
+        @endif
+
+        @if (isset($button))
+            {{ $button }}
+        @endif
+
+    </div>
+    @if (isset($title) || isset($button))
         <hr />
     @endif
     {{ $slot }}
